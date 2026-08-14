62ca3538ea480b45b2d8ddd0969e7a7a5b4a992e
aashish-ranjan <aashishranjan2011@gmail.com>
2026-08-14 23:51:04 +0530

    define app theme

--- app/src/main/java/com/aashish/writetime/MainActivity.kt
+++ app/src/main/java/com/aashish/writetime/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.aashish.writetime.ui.theme.WriteTimeTheme
+import com.aashish.writetime.common.ui.theme.WriteTimeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
