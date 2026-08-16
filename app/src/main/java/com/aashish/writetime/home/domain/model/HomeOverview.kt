acdacad4cef2f72b1cc512a56990af343691d13a
aashish-ranjan <aashishranjan2011@gmail.com>
2026-08-17 04:24:30 +0530

    remove unused files

--- app/src/main/java/com/aashish/writetime/home/domain/model/HomeOverview.kt
+++
@@ -1,8 +0,0 @@
-package com.aashish.writetime.home.domain.model
-
-import com.aashish.writetime.common.domain.model.EarningOverview
-
-data class HomeOverview(
-    val earningOverview: EarningOverview,
-    val sessionsOverview: SessionsOverview
-)
--- app/src/main/java/com/aashish/writetime/weekoverview/domain/model/WeeklyOverview.kt
+++
@@ -1,6 +0,0 @@
-package com.aashish.writetime.weekoverview.domain.model
-
-data class WeeklyOverview(
-    val earningOverview: WeeklyEarningOverview,
-    val sessionOverview: WeeklySessionOverview
-)
